06e8def53820a954d92719419098e9c37ef8390b
Will Brown <will@rebeagle.com>
2019-06-28 14:52:28 +0000

    adding updated test apps

---
+++ src/testapp/testapp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Principal;
+using System.Web.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace testapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Headers = GetHeaders();
+            return View();
+        }
+
+        public ActionResult Api()
+        {
+            var user = (WindowsIdentity)User.Identity;
+            var data = new
+            {
+                Message = "Hello",
+                user.Name,
+                user.IsAuthenticated,
+                user.AuthenticationType,
+                ImpersonationLevel = user.ImpersonationLevel.ToString(),
+                Headers = GetHeaders()
+            };
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        private Dictionary<string, string> GetHeaders()
+        {
+            return Request.Headers.AllKeys.ToDictionary(k => k, k => Request.Headers[k]);
+        }
+    }
+}
---
+++ src/testapp/testappmid/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Principal;
+using System.Web.Mvc;
+
+namespace testapp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var user = (WindowsIdentity)User.Identity;
+            ViewBag.Headers = GetHeaders();
+            ViewBag.RemoteData = GetRemoteData(user);
+            return View();
+        }
+
+        public ActionResult Api()
+        {
+            var user = (WindowsIdentity)User.Identity;
+
+            var localData = new
+            {
+                Message = "Hello",
+                user.Name,
+                user.IsAuthenticated,
+                user.AuthenticationType,
+                ImpersonationLevel = user.ImpersonationLevel.ToString(),
+                Headers = GetHeaders()
+            };
+
+            JObject remoteData = GetRemoteData(user);
+
+            var data = new
+            {
+                IncomingUser = localData,
+                BackendUser = remoteData
+            };
+
+            return Content(JsonConvert.SerializeObject(data), "application/json");
+        }
+
+        private static JObject GetRemoteData(WindowsIdentity user)
+        {
+            try
+            {
+                string remoteDataString;
+
+                using (user.Impersonate())
+                {
+                    var request = WebRequest.Create("http://testapp/api");
+                    request.UseDefaultCredentials = true;
+                    request.ImpersonationLevel = TokenImpersonationLevel.Impersonation;
+                    var response = request.GetResponse();
+                    Stream dataStream = response.GetResponseStream();
+                    var reader = new StreamReader(dataStream);
+                    remoteDataString = reader.ReadToEnd();
+                    reader.Close();
+                    dataStream.Close();
+                    response.Close();
+                }
+
+                return JObject.Parse(remoteDataString);
+            }
+            catch (Exception ex)
+            {
+                return new JObject
+                {
+                    new JProperty("error", ex.ToString())
+                };
+            }
+        }
+
+        private Dictionary<string, string> GetHeaders()
+        {
+            return Request.Headers.AllKeys.ToDictionary(k => k, k => Request.Headers[k]);
+        }
+    }
+}
